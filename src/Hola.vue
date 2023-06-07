76b29803faaf74f7fa45871d898d1855a2c28b99
Marco Antonio Hernández Nevárez <marcohdez500@hotmail.com>
2023-06-07 16:22:08 -0600

    Vue progresivo

---
+++ src/Hola.vue
@@ -0,0 +1,24 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+    <div>
+        <p>lorem ipsum</p>
+        <div class="hola">{{ text }}</div>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            text: 'Hola Vue'
+        };
+    }
+}
+</script>
+
+<style>
+.hola {
+    color: mediumseagreen;
+    font-size: large;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,8 @@
+/* import './assets/main.css'
+ */
+import { createApp } from 'vue'
+/* import App from './App.vue' */
+
+/* createApp(App).mount('#app') */
+import Hola from './Hola.vue'
+createApp(Hola).mount('#app');
